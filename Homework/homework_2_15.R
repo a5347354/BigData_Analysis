117be626e95196dcf6bc5b70d9bf0ed1169ed6aa
Apollo <a5347354@gmail.com>
2016-07-27 16:45:58 +0800

    1.Homework2 2.整理資料夾

--- Homework/homework_2_15.R
+++ Homework/homework_2_15.R
@@ -1,9 +1,3 @@
----
-title: "Homework_2_范峻豪"
-author: "Luke Fan"
-date: "2016年7月23日"
-output: html_document
----
 
 # 請使用 R 語言分析一知名電商網站的商品購買記錄purchase.csv,希望能夠從記錄之中掌握使用者的行為與商品特性。
 ## 學員需要從網路上下載該電商的使用者購買紀錄 
@@ -13,22 +7,42 @@ output: html_document
 ## 學員需要找出購買金額排行前三名的使用者ID。
 ## 學員需要根據使用者購買金額,將前十名使用者的購買金額繪製成長條圖。 (X軸為使用者ID, Y 軸為購買金額),並將該長條圖由大到小做排列。
 
-```{r}
 download.file('https://github.com/ywchiu/rtibame/raw/master/Data/purchase.csv', 'purchase.csv')
 purchase <- read.csv('purchase.csv', header=TRUE)
-
+View(purchase)
 library(dplyr)
+
+## 學員需要找出用戶在每天哪個時段(0~24)會購買最多商品。
 purchaseTimes = purchase$Time %>% as.POSIXct(., format="%Y-%m-%d %H:%M:%S")
 purchaseHours = format(purchaseTimes, "%H") %>% as.numeric() %>% table()
+#修改margin
 par(mar=c(1,1,1,1), mfrow = c(2,1))
-purchaseHours[[1]][2]
-View(purchaseHours)
+purchaseHours[[1]][1]
 barplot(purchaseHours)
+## 學員需要將每小時的購買紀錄繪製成折線圖(X軸為時段、Y軸為該時段購買 商品數量總和)。
 
 library(plotly)
 x = list(title="時段")
 y = list(title="購買量")
 plot_ly(x = array(1:24), y = purchaseHours, fill = "tozeroy", name="taipei")  %>% layout(yaxis = y, xaxis = x , title = "UXXX電商")
 
-```
+
+## 學員需要找出購買金額排行前三名的使用者ID。
+# 先做分組統計
+purchaseSum = purchase %>% group_by(User) %>% summarise(view_sum = sum(Price, na.rm=TRUE)) %>% arrange(view_sum)
+# 排序並取出前三
+top3 = purchaseSum[order(purchaseSum$view_sum,decreasing = TRUE),] %>% head(.,3)
+top3
+
+## 學員需要根據使用者購買金額,將前十名使用者的購買金額繪製成長條圖。 (X軸為使用者ID, Y 軸為購買金額),並將該長條圖由大到小做排列。
+top10 = purchaseSum[order(purchaseSum$view_sum,decreasing = TRUE),] %>% head(.,10)
+names(top10)[names(top10)=="User"]= ""
+top10
+User = top10$User
+Total = top10$view_sum
+plot_ly(x = User, y = Total,type = "bar")  %>% layout(title = "前十名使用者購買金額")
+
+
+
+
 
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ### use R
 ## 2016/07/10 R Craker_Advaned爬蟲進階
 > *https://rpubs.com/a5347354/194942
-### 2016/07/10 R Homework
+### 2016/07/17 R Homework
 > *https://rpubs.com/a5347354/195541
 
 ## 2016/07/23 R Data Visualization資料視覺化
@@ -10,5 +10,6 @@
 
 ## 2016/07/24 R With DataBase-MySQL連接資料庫
 > *https://rpubs.com/a5347354/198011
-### 2016/07/10 R Homework 2
+### 2016/07/24 R Homework 2
+> *請使用 R 語言分析一知名電商網站的商品購買記錄purchase.csv,希望能夠從記錄之中掌握使用者的行為與商品特性。
 > *https://rpubs.com/a5347354/198194
