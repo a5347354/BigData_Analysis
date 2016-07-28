941841b23829f119873716a6a5931a59b6242a68
Apollo <a5347354@gmail.com>
2016-07-28 22:36:58 +0800

    更新20160724當天未完成的課程內容-連接MySQL及其操作

--- Homework/homework_2_15.R
+++ Homework/homework_2_15.R
@@ -1,11 +1,13 @@
 
 # 請使用 R 語言分析一知名電商網站的商品購買記錄purchase.csv,希望能夠從記錄之中掌握使用者的行為與商品特性。
 ## 學員需要從網路上下載該電商的使用者購買紀錄 
-## purchase <- read.csv(‘purchase.csv', header=TRUE)
+## purchase <- read.csv(‘purchase.csv', header=TRUE)
 ## 學員需要找出用戶在每天哪個時段(0~24)會購買最多商品。
 ## 學員需要將每小時的購買紀錄繪製成折線圖(X軸為時段、Y軸為該時段購買 商品數量總和)。
 ## 學員需要找出購買金額排行前三名的使用者ID。
 ## 學員需要根據使用者購買金額,將前十名使用者的購買金額繪製成長條圖。 (X軸為使用者ID, Y 軸為購買金額),並將該長條圖由大到小做排列。
+
+#-----https://rpubs.com/a5347354/198194-----
 
 download.file('https://github.com/ywchiu/rtibame/raw/master/Data/purchase.csv', 'purchase.csv')
 purchase <- read.csv('purchase.csv', header=TRUE)
