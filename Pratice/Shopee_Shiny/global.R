3fe56ef708cab0583f91f39b16acc58901e79252
Apollo <a5347354@gmail.com>
2016-08-18 13:30:11 +0800

    finished ToSellLikeHotcakes version 1.0

--- Pratice/Shopee_Shiny/global.R
+++ Pratice/Shopee_Shiny/global.R
@@ -1,3 +1,4 @@
+library(rvest)
 orders = read.csv("korean10146.shopee-order.csv")
 orders = orders[,-c(1,2,3,4,11,12,13,17)]
 as.character.Date(orders$訂單成立時間)
@@ -17,4 +18,45 @@ products = c("養樂多軟糖",
              "檸檬片",
              "黑旋風巧克力棒",
              "香蕉巧克力派") 
+
+
+#-------------熱銷商品_Crawler---------------
+#抓取資料
+library(jsonlite)
+datas_menu = function (data_count){
+  data_count = (data_count - 20) / 20 *50
+  i = 0
+  while( i < data_count ){
+    tag = "sales"
+    url = paste0("http://mall.shopee.tw/search/api/items/?page_type=search&match_id=2209&keyword=%E9%80%B2%E5%8F%A3%E9%9B%B6%E9%A3%9F&shop_categoryids=&hashtag=&facet_type=&by=",tag,"&order=desc&newest=",i,"&limit=20&need_drop_word=false")
+    menu = fromJSON(url)
+    items = menu$items[c('name','price','stock','rating_good','liked_count','rating_star')]
+    items = items
+    if(i == 0){
+      i = i + 20
+      old_items = items
+    }
+    else{
+      i = i + 50
+      old_items = rbind(old_items,items)
+    }
+  }
+  return(old_items)
+}
+
+
+#資料處理
+# menu$items
+# name 商品名稱
+# price / 100000 價錢
+# rating_good 評價
+# liked_count 讚數
+# rating_star 廠商評價
+# stock 存貨
+#datas_menu(抓取資料筆數)
+
+items = datas_menu(200)
+items$price = as.numeric(items$price) / 100000
+
+# View(items[grepl('小雞麵',items$name),])
 
--- Pratice/Shopee_Shiny/ui.R
+++ Pratice/Shopee_Shiny/ui.R
@@ -4,43 +4,57 @@ shinyUI(
   fluidPage( 
     #第一層
     titlePanel("Shopee Anaysis"),
-    
-    #第二層
-    fluidRow(
-      splitLayout(cellWidths = c("30%","70%"),
-        wellPanel(
-          h4('Filter:'), 
-          checkboxGroupInput("categories",
-                             "Category", 
-                             categories,
-                             selected = categories)
+    #第二層標籤（spilt work）
+    tabsetPanel(
+      tabPanel("銷售",
+        fluidRow(
+          splitLayout(cellWidths = c("30%","70%"),
+            wellPanel(
+              h4('Filter:'), 
+              checkboxGroupInput("categories",
+                                 "Category", 
+                                 categories,
+                                 selected = categories)
+            ),
+            wellPanel(
+              h4('Keyword Volume:'), 
+              br(), 
+              textInput("keyword",
+                         "Search", width='250px', placeholder="品客" ),
+              htmlOutput("volume_order") 
+            )
+          )
         ),
-        wellPanel(
-          h4('Keyword Volume:'), 
-          br(), 
-          textInput("keyword",
-                     "Search", width='250px', placeholder="品客" ),
-          htmlOutput("volume") 
+      
+        
+        #第三層
+        splitLayout(cellWidths = c("30%","70%"),
+          dateRangeInput("dates", label = h3("Date range"),start = "2016-07-10"),
+          
+          # Tab標籤
+          tabsetPanel(
+            tabPanel("Summary",plotlyOutput("plotly_markers")), 
+            tabPanel("各小時訂單分佈",plotlyOutput("plotly_rect")), 
+            tabPanel("各產品銷量及銷售額",plotlyOutput("plotly_bar")),
+            tabPanel("訂單", tableOutput("itemtable"))
+          ) 
         )
-      )
-    ),
-    
-    
-    
-    
-    #第三層
-    splitLayout(cellWidths = c("30%","70%"),
-      dateRangeInput("dates", label = h3("Date range"),start = "2016-07-10"),
+      ),
       
       
-        # Tab標籤
-        tabsetPanel(
-          tabPanel("Summary",plotlyOutput("plotly_markers")), 
-          tabPanel("各小時訂單分佈",plotlyOutput("plotly_rect")), 
-          tabPanel("各產品銷量及銷售額",plotlyOutput("plotly_bar")),
-          tabPanel("訂單", tableOutput("itemtable"))
-        ) 
-    
+      tabPanel("熱銷商品",
+               fluidRow(
+                 wellPanel(
+                   h4('Keyword Volume:'), 
+                   br(), 
+                   textInput("marketplace_keyword",
+                             "Search", width='250px', placeholder="品客" ),
+                   htmlOutput("marketplace_volume") 
+                 ),
+                 tableOutput("marketplace_table"),
+                 plotlyOutput("market_plotly_bar")
+               )
+              )
     )
   )
 )
